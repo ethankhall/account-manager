1a8894807b8203638987b1a0cf5fcff502ba3dba
Ethan Hall <ethan@ehdev.io>
2018-11-17 18:02:23 -0800

    Default to http

--- webapp/src/main/kotlin/io/ehdev/account/web/configuration/extension.kt
+++ webapp/src/main/kotlin/io/ehdev/account/web/configuration/extension.kt
@@ -3,5 +3,5 @@ package io.ehdev.account.web.configuration
 import org.springframework.web.reactive.function.server.ServerRequest
 
 fun ServerRequest.findScheme(): String {
-    return this.headers().header("X-Forwarded-Proto").first() ?: this.uri().scheme
+    return this.headers().header("X-Forwarded-Proto").firstOrNull() ?: (this.uri().scheme ?: "http")
 }
