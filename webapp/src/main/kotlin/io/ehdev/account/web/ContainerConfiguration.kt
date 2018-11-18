511edb732a03f019f9e33a99379602d469ce7eb1
Ethan Hall <ethan@ehdev.io>
2018-11-17 21:26:36 -0800

    Finally fixing redis

--- webapp/src/main/kotlin/io/ehdev/account/web/ContainerConfiguration.kt
+++ webapp/src/main/kotlin/io/ehdev/account/web/ContainerConfiguration.kt
@@ -3,14 +3,11 @@ package io.ehdev.account.web
 import io.ehdev.account.web.configuration.ApplicationRoutesConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.scheduling.annotation.SchedulingConfiguration
-import org.springframework.session.ReactiveMapSessionRepository
-import org.springframework.session.config.annotation.web.server.EnableSpringWebSession
+import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession
 import org.springframework.web.reactive.config.EnableWebFlux
-import java.util.concurrent.ConcurrentHashMap
 
 @Import(
         DataSourceAutoConfiguration::class,
@@ -20,9 +17,5 @@ import java.util.concurrent.ConcurrentHashMap
 @Configuration
 @EnableWebFlux
 @SpringBootApplication
-@EnableSpringWebSession
-open class ContainerConfiguration {
-
-    @Bean
-    open fun reactiveSessionRepository() = ReactiveMapSessionRepository(ConcurrentHashMap(20))
-}
+@EnableRedisWebSession
+open class ContainerConfiguration
