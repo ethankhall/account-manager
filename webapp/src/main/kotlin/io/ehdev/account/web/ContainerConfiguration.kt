bb7662254f319be878053b1615d6c4417701ebcb
Ethan Hall <ethankhall@pm.me>
2019-03-13 15:42:17 -0700

    Allow graphite to be disabled

--- webapp/src/main/kotlin/io/ehdev/account/web/ContainerConfiguration.kt
+++ webapp/src/main/kotlin/io/ehdev/account/web/ContainerConfiguration.kt
@@ -6,6 +6,7 @@ import io.micrometer.core.instrument.util.HierarchicalNameMapper
 import io.micrometer.graphite.GraphiteConfig
 import io.micrometer.graphite.GraphiteMeterRegistry
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -25,6 +26,7 @@ import org.springframework.web.reactive.config.EnableWebFlux
 open class ContainerConfiguration {
 
     @Bean
+    @ConditionalOnBean(GraphiteConfig::class)
     open fun graphiteMeterRegistry(config: GraphiteConfig, clock: Clock, env: Environment): GraphiteMeterRegistry {
         val prefix = env.getProperty("metric.prefix", "account-manager")
 
